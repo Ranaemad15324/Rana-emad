7708fec067c9550931dd3cdf13984f159ff9fafb
Rana emad <s241172@ese.gov.ae>
2023-06-24 13:57:45 +0400

    Initial Commit

---
+++ Rana emad/Rana_emadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rana_emadApp.swift
+//  Rana emad
+//
+//  Created by Rana Emad on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Rana_emadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
